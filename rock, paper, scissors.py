e4148f3dc37e555fa6f3815961a3404785e020b6
mustbe23 <2310631230016@student.unsika.ac.id>
2024-08-23 20:38:31 +0700

    Add files via upload

---
+++ rock, paper, scissors.py
@@ -0,0 +1,39 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+option = ["rock", "paper", "scissors"]          
+
+while True:
+    user_input = input("type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+            break
+
+    if user_input not in option:
+          continue  
+    
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+    computer_pick = option[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+    
+    else:
+         print("you lost!")
+         computer_wins += 1
+
+print("you won", user_wins, "times")
+print("the computer won", computer_wins)
+print("Goodbye!")
